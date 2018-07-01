ab1b78d5b28973b336f45b7fa55ffdb9c18f6f70
aiman1717a <aiman1717a@gmail.com>
2018-07-01 19:47:56 +0530

    commit 1 by aiman

---
+++ eShopping/form_signin.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace eShopping
+{
+    partial class form_signin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_signin));
+            this.txt_u_username = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txt_u_password = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.bunifuCards1 = new Bunifu.Framework.UI.BunifuCards();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.btn_r_signup = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btn_u_login = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.txt_r_conpasswword = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txt_r_password = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txt_r_username = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.bunifuSeparator1 = new Bunifu.Framework.UI.BunifuSeparator();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbl_userTableAdapter1 = new eShopping.DataSet1TableAdapters.tbl_userTableAdapter();
+            this.bunifuCards1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_u_username
+            // 
+            this.txt_u_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_u_username.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_u_username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_u_username.HintForeColor = System.Drawing.Color.Black;
+            this.txt_u_username.HintText = "User Name";
+            this.txt_u_username.isPassword = false;
+            this.txt_u_username.LineFocusedColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_u_username.LineIdleColor = System.Drawing.Color.Gray;
+            this.txt_u_username.LineMouseHoverColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_u_username.LineThickness = 3;
+            this.txt_u_username.Location = new System.Drawing.Point(426, 150);
+            this.txt_u_username.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_u_username.Name = "txt_u_username";
+            this.txt_u_username.Size = new System.Drawing.Size(237, 33);
+            this.txt_u_username.TabIndex = 1;
+            this.txt_u_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // txt_u_password
+            // 
+            this.txt_u_password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_u_password.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_u_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_u_password.HintForeColor = System.Drawing.Color.Black;
+            this.txt_u_password.HintText = "Password";
+            this.txt_u_password.isPassword = false;
+            this.txt_u_password.LineFocusedColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_u_password.LineIdleColor = System.Drawing.Color.Gray;
+            this.txt_u_password.LineMouseHoverColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_u_password.LineThickness = 3;
+            this.txt_u_password.Location = new System.Drawing.Point(426, 209);
+            this.txt_u_password.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_u_password.Name = "txt_u_password";
+            this.txt_u_password.Size = new System.Drawing.Size(237, 33);
+            this.txt_u_password.TabIndex = 2;
+            this.txt_u_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // bunifuCards1
+            // 
+            this.bunifuCards1.BackColor = System.Drawing.Color.White;
+            this.bunifuCards1.BorderRadius = 5;
+            this.bunifuCards1.BottomSahddow = true;
+            this.bunifuCards1.color = System.Drawing.Color.SkyBlue;
+            this.bunifuCards1.Controls.Add(this.bunifuCustomLabel2);
+            this.bunifuCards1.Controls.Add(this.bunifuCustomLabel1);
+            this.bunifuCards1.Controls.Add(this.btn_r_signup);
+            this.bunifuCards1.Controls.Add(this.btn_u_login);
+            this.bunifuCards1.Controls.Add(this.txt_r_conpasswword);
+            this.bunifuCards1.Controls.Add(this.txt_r_password);
+            this.bunifuCards1.Controls.Add(this.txt_u_password);
+            this.bunifuCards1.Controls.Add(this.txt_r_username);
+            this.bunifuCards1.Controls.Add(this.txt_u_username);
+            this.bunifuCards1.Controls.Add(this.bunifuSeparator1);
+            this.bunifuCards1.LeftSahddow = false;
+            this.bunifuCards1.Location = new System.Drawing.Point(201, 106);
+            this.bunifuCards1.Name = "bunifuCards1";
+            this.bunifuCards1.RightSahddow = true;
+            this.bunifuCards1.ShadowDepth = 20;
+            this.bunifuCards1.Size = new System.Drawing.Size(717, 477);
+            this.bunifuCards1.TabIndex = 3;
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(111, 68);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(134, 39);
+            this.bunifuCustomLabel2.TabIndex = 4;
+            this.bunifuCustomLabel2.Text = "Sign Up";
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(484, 68);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(102, 39);
+            this.bunifuCustomLabel1.TabIndex = 4;
+            this.bunifuCustomLabel1.Text = "Login";
+            // 
+            // btn_r_signup
+            // 
+            this.btn_r_signup.ActiveBorderThickness = 1;
+            this.btn_r_signup.ActiveCornerRadius = 20;
+            this.btn_r_signup.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btn_r_signup.ActiveForecolor = System.Drawing.Color.White;
+            this.btn_r_signup.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btn_r_signup.BackColor = System.Drawing.Color.White;
+            this.btn_r_signup.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_r_signup.BackgroundImage")));
+            this.btn_r_signup.ButtonText = "Sign Up";
+            this.btn_r_signup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_r_signup.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_r_signup.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btn_r_signup.IdleBorderThickness = 1;
+            this.btn_r_signup.IdleCornerRadius = 20;
+            this.btn_r_signup.IdleFillColor = System.Drawing.Color.White;
+            this.btn_r_signup.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btn_r_signup.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btn_r_signup.Location = new System.Drawing.Point(65, 334);
+            this.btn_r_signup.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_r_signup.Name = "btn_r_signup";
+            this.btn_r_signup.Size = new System.Drawing.Size(237, 52);
+            this.btn_r_signup.TabIndex = 7;
+            this.btn_r_signup.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_r_signup.Click += new System.EventHandler(this.btn_r_signup_Click);
+            // 
+            // btn_u_login
+            // 
+            this.btn_u_login.ActiveBorderThickness = 1;
+            this.btn_u_login.ActiveCornerRadius = 20;
+            this.btn_u_login.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btn_u_login.ActiveForecolor = System.Drawing.Color.White;
+            this.btn_u_login.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btn_u_login.BackColor = System.Drawing.Color.White;
+            this.btn_u_login.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_u_login.BackgroundImage")));
+            this.btn_u_login.ButtonText = "Login";
+            this.btn_u_login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_u_login.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_u_login.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btn_u_login.IdleBorderThickness = 1;
+            this.btn_u_login.IdleCornerRadius = 20;
+            this.btn_u_login.IdleFillColor = System.Drawing.Color.White;
+            this.btn_u_login.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btn_u_login.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btn_u_login.Location = new System.Drawing.Point(426, 251);
+            this.btn_u_login.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_u_login.Name = "btn_u_login";
+            this.btn_u_login.Size = new System.Drawing.Size(237, 52);
+            this.btn_u_login.TabIndex = 3;
+            this.btn_u_login.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_u_login.Click += new System.EventHandler(this.btn_u_login_Click);
+            // 
+            // txt_r_conpasswword
+            // 
+            this.txt_r_conpasswword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_r_conpasswword.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_r_conpasswword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_r_conpasswword.HintForeColor = System.Drawing.Color.Black;
+            this.txt_r_conpasswword.HintText = "Confirm Password";
+            this.txt_r_conpasswword.isPassword = false;
+            this.txt_r_conpasswword.LineFocusedColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_r_conpasswword.LineIdleColor = System.Drawing.Color.Gray;
+            this.txt_r_conpasswword.LineMouseHoverColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_r_conpasswword.LineThickness = 3;
+            this.txt_r_conpasswword.Location = new System.Drawing.Point(65, 265);
+            this.txt_r_conpasswword.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_r_conpasswword.Name = "txt_r_conpasswword";
+            this.txt_r_conpasswword.Size = new System.Drawing.Size(237, 33);
+            this.txt_r_conpasswword.TabIndex = 6;
+            this.txt_r_conpasswword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // txt_r_password
+            // 
+            this.txt_r_password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_r_password.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_r_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_r_password.HintForeColor = System.Drawing.Color.Black;
+            this.txt_r_password.HintText = "Password";
+            this.txt_r_password.isPassword = false;
+            this.txt_r_password.LineFocusedColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_r_password.LineIdleColor = System.Drawing.Color.Gray;
+            this.txt_r_password.LineMouseHoverColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_r_password.LineThickness = 3;
+            this.txt_r_password.Location = new System.Drawing.Point(65, 209);
+            this.txt_r_password.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_r_password.Name = "txt_r_password";
+            this.txt_r_password.Size = new System.Drawing.Size(237, 33);
+            this.txt_r_password.TabIndex = 5;
+            this.txt_r_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // txt_r_username
+            // 
+            this.txt_r_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_r_username.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txt_r_username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_r_username.HintForeColor = System.Drawing.Color.Black;
+            this.txt_r_username.HintText = "User Name";
+            this.txt_r_username.isPassword = false;
+            this.txt_r_username.LineFocusedColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_r_username.LineIdleColor = System.Drawing.Color.Gray;
+            this.txt_r_username.LineMouseHoverColor = System.Drawing.Color.DeepSkyBlue;
+            this.txt_r_username.LineThickness = 3;
+            this.txt_r_username.Location = new System.Drawing.Point(65, 150);
+            this.txt_r_username.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_r_username.Name = "txt_r_username";
+            this.txt_r_username.Size = new System.Drawing.Size(237, 33);
+            this.txt_r_username.TabIndex = 4;
+            this.txt_r_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // bunifuSeparator1
+            // 
+            this.bunifuSeparator1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.bunifuSeparator1.LineThickness = 5;
+            this.bunifuSeparator1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuSeparator1.Name = "bunifuSeparator1";
+            this.bunifuSeparator1.Size = new System.Drawing.Size(717, 477);
+            this.bunifuSeparator1.TabIndex = 5;
+            this.bunifuSeparator1.Transparency = 255;
+            this.bunifuSeparator1.Vertical = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1118, 699);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbl_userTableAdapter1
+            // 
+            this.tbl_userTableAdapter1.ClearBeforeFill = true;
+            // 
+            // form_signin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1118, 699);
+            this.Controls.Add(this.bunifuCards1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "form_signin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "E-Shopping";
+            this.bunifuCards1.ResumeLayout(false);
+            this.bunifuCards1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_u_username;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_u_password;
+        private Bunifu.Framework.UI.BunifuCards bunifuCards1;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn_u_login;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn_r_signup;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_r_password;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_r_username;
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_r_conpasswword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private DataSet1TableAdapters.tbl_userTableAdapter tbl_userTableAdapter1;
+    }
+}
---
+++ eShopping/form_signin.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eShopping
+{
+    public partial class form_signin : Form
+    {
+        public form_signin()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_u_login_Click(object sender, EventArgs e)
+        {
+            bool checker = this.login(txt_u_username.Text, txt_u_password.Text);
+            if (checker == true)
+            {
+                MessageBox.Show("You have logged in");
+            }
+            else
+            {
+                MessageBox.Show("wrong Information");
+            }
+
+        }
+        private bool login(string uname, string upass)
+        {
+            int count = tbl_userTableAdapter1.GetDataForLogin(uname, upass).Rows.Count;
+            if (count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btn_r_signup_Click(object sender, EventArgs e)
+        {
+            bool validation_cheker = this.r_validation(txt_r_password.Text, txt_r_conpasswword.Text);
+            if (validation_cheker == true)
+            {
+                bool checker = this.registration(txt_r_username.Text, txt_r_password.Text);
+                if (checker == true)
+                {
+                    MessageBox.Show("You Have signed Up");
+                }
+            }
+            else
+            {
+                MessageBox.Show("password not matching");
+            }
+        }
+        private bool r_validation(string rpass, string rconpass)
+        {
+            if(rpass == rconpass)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private bool registration(string rname, string rpass)
+        {
+            int checker = tbl_userTableAdapter1.InsertQueryforRegistration(rname, rpass);
+            if (checker > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
